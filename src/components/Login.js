685fb5099c4a082b105dff78054f0a2a5197ed71
Abhishreevk <abhishreekanoj02@gmail.com>
2020-07-10 16:09:17 +0530

    login updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,35 @@
 import { h, Component } from 'preact';
 
+
+
+
 class Login extends Component {
-    render({}, {}) {
+    state = { value: '' };
+    onSubmit = e => {
+        e.preventDefault();
+        console.log("onsubmit",e)
+    }
+
+    onInput = e => {
+        const { value } = e.target;
+        this.setState({ value })
+        console.log("oninput value",this.state.value)
+    }
+    render({ }, { value }) {
         return (
             <div>
-                Login
+                <h2>Login</h2>
+                <form onSubmit={this.onSubmit}>
+                    <div className="row">
+                    <label>Username: </label>
+                    <input type="text" value={value} onInput={this.onInput} />
+                    </div>
+                    <div className="row">
+                    <label>Password: </label>
+                    <input type="password" value={value} onInput={this.onInput} />
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
